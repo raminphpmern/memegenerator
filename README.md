dd5aedced3d113315eb786f0d4e8a70824842981
Ramkumar <ramkumar87.bca@gmail.com>
2023-03-26 19:15:11 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
-# Create a Registration and Login System with PHP and MySQL
-Create a Registration and Login System with PHP and MySQL
+Create your Own Memes and use our existing images to download the Generated Images.
 
-Article: https://speedysense.com/create-registration-login-system-php-mysql
+Register and login to access more features from our Meme Generator Portal in minutes
+
+Folder name should msc-project/meme-creator-web
+
+Mysql file be placed in the root folder of the respective branch file name will be meme_generator.sql 
---
+++ meme_generator.sql
@@ -0,0 +1,69 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 26, 2023 at 03:44 PM
+-- Server version: 10.4.22-MariaDB
+-- PHP Version: 8.1.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `meme_generator`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `create_datetime` date NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `email`, `password`, `create_datetime`) VALUES
+(1, 'ram007', 'ramkumar87.bca@gmail.com', 'bf3c4b6c96908561fe1742b9fb5c3f11', '2023-03-21'),
+(2, 'balaji', 'balajismart01@gmail.com', 'bf3c4b6c96908561fe1742b9fb5c3f11', '2023-03-21');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
