5b13200a0e69be36bf3c5d73eb8de193a54aa65b
Ramkumar <ramkumar87.bca@gmail.com>
2023-03-26 19:20:33 +0530

    Add files via upload
    Changes in DB connection variables

--- db.php
+++ db.php
@@ -1,7 +1,11 @@
 <?php
     // Enter your host name, database username, password, and database name.
     // If you have not set database password on localhost then set empty.
-    $con = mysqli_connect("localhost","root","","meme_generator");
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "meme_generator";
+    $con = mysqli_connect($servername, $username, $password,$dbname);
     // Check connection
     if (mysqli_connect_errno()){
         echo "Failed to connect to MySQL: " . mysqli_connect_error();
